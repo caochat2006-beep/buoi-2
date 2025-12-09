6a90fc3d14cd286e341d9ffabe85313b558a6135
caochat2006-beep <caochat2006@gmail.com>
2025-12-09 10:49:16 +0700

    Add files via upload

---
+++ 1.4.9 (1).py
@@ -0,0 +1,18 @@
+print("sv:Cao Van Chat")
+print("msv:245752021610122")
+print("###########################")
+# Nhập chuỗi
+s = input("Nhập vào một chuỗi: ")
+
+# Tạo dictionary rỗng để lưu kết quả
+char_count = {}
+
+# Duyệt từng ký tự trong chuỗi
+for ch in s:
+    if ch in char_count:
+        char_count[ch] += 1
+    else:
+        char_count[ch] = 1
+
+# In kết quả
+print(char_count)
